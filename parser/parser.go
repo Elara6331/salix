a465be6cf8c23e35d3e4a85da68034df883b7b3c
Elara6331 <elara@elara.ws>
2023-12-17 18:15:13 -0800

    Switch to MPL-2.0 to allow static linking

--- parser/parser.go
+++ parser/parser.go
@@ -309,7 +309,7 @@ var g = &grammar{
 					},
 				},
 			},
-			leader:        true,
+			leader:        false,
 			leftRecursive: true,
 		},
 		{
@@ -1303,7 +1303,7 @@ var g = &grammar{
 					},
 				},
 			},
-			leader:        false,
+			leader:        true,
 			leftRecursive: true,
 		},
 		{
