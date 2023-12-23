b6c177c3d26294d9bc461133499efc5bc4009bcf
Elara6331 <elara@elara.ws>
2023-12-22 19:30:07 -0800

    Regenerate parser

--- parser/parser.go
+++ parser/parser.go
@@ -309,7 +309,7 @@ var g = &grammar{
 					},
 				},
 			},
-			leader:        true,
+			leader:        false,
 			leftRecursive: true,
 		},
 		{
@@ -1309,7 +1309,7 @@ var g = &grammar{
 					},
 				},
 			},
-			leader:        false,
+			leader:        true,
 			leftRecursive: true,
 		},
 		{
